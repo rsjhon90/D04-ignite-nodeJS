1c6334d879d0ea8f73a9bdbf660f685d9785ead3
rsjhon90 <rsjhony@hotmail.com>
2021-08-18 18:10:27 -0300

    Desafio Finalizado

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,51 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    const newUser = Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(newUser);
+
+    return newUser;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = this.users.find((user) => {
+      const userFound = user.id === receivedUser.id;
+
+      if (userFound) {
+        // eslint-disable-next-line no-param-reassign
+        user.admin = true;
+        // eslint-disable-next-line no-param-reassign
+        user.updated_at = new Date();
+      }
+
+      return userFound;
+    });
+
+    return user;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,7 +1,9 @@
+import { validate as isUuid } from "uuid";
+
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
-interface IRequest {
+export interface IRequest {
   user_id: string;
 }
 
@@ -9,7 +11,21 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!isUuid(user_id)) {
+      throw new Error("User does not exists!2");
+    }
+    if (!user) {
+      throw new Error("User does not exists!");
+    }
+
+    if (!user.admin) {
+      throw new Error("User is not admin!");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
